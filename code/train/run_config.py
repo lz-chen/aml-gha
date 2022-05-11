811d95a9b5f7222db0939df7508ac55ee77d19c0
lz-chen <lingzhen.chen@inmeta.no>
2022-05-11 16:05:58 +0200

    train param from env

--- code/train/run_config.py
+++ code/train/run_config.py
@@ -1,15 +1,20 @@
+import os
+
 from azureml.core import ComputeTarget
 from azureml.train.estimator import Estimator
 
 
 def main(workspace):
+    kernel = os.environ.get("KERNEL_TYPE", "linear")
+    penalty = int(os.environ.get("PENALTY", 1.0))
+
     # Load compute target
     print("Loading compute target")
     compute_target = ComputeTarget(workspace=workspace, name="cpu-cluster")
 
     # Load script parameters
     print("Loading script parameters")
-    script_params = {"--kernel": "linear", "--penalty": 1.0}
+    script_params = {"--kernel": kernel, "--penalty": penalty}
 
     # Create experiment config
     print("Creating experiment config")
