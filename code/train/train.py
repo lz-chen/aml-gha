cb6a0c944fda2c28950c957021d62a2e717a43f4
Lingzhen Chen <38908263+lz-chen@users.noreply.github.com>
2022-05-13 12:54:49 +0200

    refactor actions (#12)
    * test change
    
    * main branch name
    
    * debug cache
    
    * try another cache
    
    * syntax
    
    * check trigger deploy
    
    * test change
    
    * change train.py
    
    * test
    
    * shorten ci
    
    * update token
    
    * change train.py
    
    * key error
    
    * change train.py
    
    * test
    
    * bug
    
    * add test
    
    * cd dispatcher don't need to run in environment
    
    * change name
    
    * remove unnecessary files
    
    * update train cmd
    
    Co-authored-by: lz-chen <lingzhen.chen@inmeta.no>

--- code/train/train.py
+++ code/train/train.py
@@ -7,7 +7,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from azureml.core import Run
 from sklearn import datasets
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.metrics import (  # precision_score, recall_score # noqa
+    confusion_matrix,
+    f1_score,
+)
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
@@ -116,15 +119,16 @@ def main(args):
     print("Accuracy of SVM classifier on test set: {:.2f}".format(accuracy))
     run.log("Accuracy", np.float(accuracy))
 
-    # precision for X_test
-    precision = precision_score(svm_predictions, data["test"]["y"], average="weighted")
-    print("Precision of SVM classifier on test set: {:.2f}".format(precision))
-    run.log("precision", precision)
-
-    # recall for X_test
-    recall = recall_score(svm_predictions, data["test"]["y"], average="weighted")
-    print("Recall of SVM classifier on test set: {:.2f}".format(recall))
-    run.log("recall", recall)
+    # # precision for X_test
+    # precision = precision_score(svm_predictions, data["test"]["y"],
+    # average="weighted")
+    # print("Precision of SVM classifier on test set: {:.2f}".format(precision))
+    # run.log("precision", precision)
+    #
+    # # recall for X_test
+    # recall = recall_score(svm_predictions, data["test"]["y"], average="weighted")
+    # print("Recall of SVM classifier on test set: {:.2f}".format(recall))
+    # run.log("recall", recall)
 
     # f1-score for X_test
     f1 = f1_score(svm_predictions, data["test"]["y"], average="weighted")
